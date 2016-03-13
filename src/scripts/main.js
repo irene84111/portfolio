aca9b75fb6764bda7d44bab19b08b2a5deb6e4f8
Irene Chang <irene84111@gmail.com>
2016-03-13 12:27:01 +0800

    fix rwd of mobile

---
+++ src/scripts/main.js
@@ -0,0 +1,32 @@
+$(".btn-about").click(function(){
+    $(".cd-panel-close-mobile").fadeIn();
+});
+$(".cd-panel-close-mobile").click(function(){
+    $(".cd-panel-close-mobile").fadeOut();
+});
+
+// ScrollReveal
+window.sr = ScrollReveal();
+var main = {
+  duration : 1500,
+  delay    : 200,
+};
+var mainLeft = {
+  duration : 1500,
+  delay    : 100,
+  distance : '90px',
+  rotate   : { z: 10 },
+  scale    : 0.8
+};
+var mainRight = {
+  duration : 1500,
+  delay    : 500,
+  distance : '90px',
+  rotate   : { z: -10 },
+  scale    : 0.8
+};
+
+sr.reveal('.about', mainLeft);
+sr.reveal('.skill', mainRight);
+sr.reveal('.work', main);
+sr.reveal('.blog', main);
