4c8a792c1067f05636fbe628c5419d234c145fec
Irene Chang <irene84111@gmail.com>
2016-03-25 11:03:55 +0800

    clean up

--- dist/scripts/scroll-reveal.js
+++
@@ -1,1 +0,0 @@
-window.sr=ScrollReveal();var main={duration:1500,delay:200},mainLeft={duration:1500,delay:100,distance:"90px",rotate:{z:10},scale:.8},mainRight={duration:1500,delay:500,distance:"90px",rotate:{z:-10},scale:.8};sr.reveal(".about",mainLeft),sr.reveal(".skill",mainRight),sr.reveal(".blog",main);
